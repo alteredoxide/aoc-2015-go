0ff0a318cf5c7a5427625b8c09eef410950b22a0
alteredoxide <me@alteredoxide.com>
2023-10-18 12:54:39 -0700

    day 3, part 1

---
+++ day3/soltuion_test.go
@@ -0,0 +1,32 @@
+package day3;
+
+import (
+    "os"
+    "path/filepath"
+    "runtime"
+    "testing"
+)
+
+
+func loadInput() (string, error) {
+    _, file, _, _ := runtime.Caller(0)
+    path := filepath.Dir(file)
+    data, err := os.ReadFile(filepath.Join(path, "input.txt"))
+    if err != nil {
+        return "", err
+    }
+    return string(data), nil
+}
+
+
+func TestPart1(t *testing.T) {
+    input, err := loadInput()
+    if err != nil {
+        t.Fatal(err)
+    }
+    output := part1(input)
+    var expected int = 2565
+    if output != expected {
+        t.Fatalf("exptected %d but found %d", expected, output)
+    }
+}
